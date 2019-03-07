fc713d5d8928ed254b127f8c5c98213002340c27
Roger Errepama <eldekornella@hotmail.com>
2019-03-07 16:52:09 +0100

    Afegit main

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	
+	public static void main ( String[] args){
+		
+		Vehicle V1 = new Vehicle (80, "123456C");
+		
+		
+		V1.engega();
+		
+		V1.accelerar();
+		
+		V1.deccelerar();
+		
+		V1.aturarse();
+		
+		
+       
+    }
+
+
+}
