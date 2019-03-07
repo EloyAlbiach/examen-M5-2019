0587355ee74544e5b8daa232ef06852c2f72e51a
cfgs <alvaro5_stb@hotmail.com>
2019-03-07 17:06:44 +0100

    commit

---
+++ prova.java
@@ -0,0 +1,14 @@
+
+public class prova {
+
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle(150, "5555 AAA");
+		v1.engega();
+		v1.acelerar();
+		v1.atura();
+		v1.frenar();
+		v1.repararVehicle();
+
+	}
+
+}
