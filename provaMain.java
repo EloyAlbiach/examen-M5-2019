b52982f6095803e1d63331397bf1b9734d79c8ad
Sergio Gomez <sergiosanvi3@gmail.com>
2019-03-07 16:32:22 +0100

    Creacio del fitxer provaMain

---
+++ provaMain.java
@@ -0,0 +1,15 @@
+import examen-M5-2019. Vehicle;
+
+public class provaMain {
+
+	public static void main(String[] args) {
+		Vehicle vehicle = new Vehicle(100, "MatriculaA");
+		vehicle.engega();
+		vehicle.acelerar();
+		vehicle.acelerar();
+		vehicle.frenar();
+		vehicle.repararVehicle();
+
+	}
+
+}
