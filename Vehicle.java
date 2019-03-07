59169448df436e573eebd0e18f7de0ce42e64315
samuelroman <sami5998@gmail.com>
2019-03-07 16:23:43 +0100

    fusionats metodes de engegar o aturar

--- Vehicle.java
+++ Vehicle.java
@@ -1,48 +1,46 @@
 public class Vehicle {
 
-    private int vLimit;
-    private int velocitatActual = 0;
-    private boolean engegat = false;
-    private String matricula;
+	private int vLimit;
+	private int velocitatActual = 0;
+	private boolean engegat = false;
+	private String matricula;
 
-    public Vehicle (int velocitatLimit, String matriculaCotxe){
-        vLimit = velocitatLimit;
-        matricula = matriculaCotxe;
-    }
+	public Vehicle(int velocitatLimit, String matriculaCotxe) {
+		vLimit = velocitatLimit;
+		matricula = matriculaCotxe;
+	}
 
-    public void engega (){
-       engegat = true;
-        printSpecs();
-    }
+	public void engegaOAtura() {
+		if (engegat) {
+			printSpecs();
+		} else {
+			System.out.println("M'he aturat");
+		}
+	}
 
 	private void printSpecs() {
 		System.out.println("Estic engegat.");
-        System.out.println("Velocitat actual = " + velocitatActual);
-        System.out.println("Límit de velocitat = " + vLimit);
-        System.out.println("Matrícula = " + matricula);
+		System.out.println("Velocitat actual = " + velocitatActual);
+		System.out.println("Límit de velocitat = " + vLimit);
+		System.out.println("Matrícula = " + matricula);
 	}
 
-    public void atura (){
-        engegat = false;
-        System.out.println("M'he aturat");
-    }
+	public void acc() {
+		if (engegat && ((velocitatActual + 10) < vLimit)) {
+			velocitatActual += 10;
+		}
+		printSpecs();
+	}
 
-    public void acc (){
-        if (engegat && ((velocitatActual + 10) < vLimit)){
-            velocitatActual += 10;
-        }
-        printSpecs();
-    }
+	public void dec() {
+		if (engegat && (velocitatActual > 0)) {
+			velocitatActual -= 10;
+		}
+		printSpecs();
+	}
 
-    public void dec (){
-        if (engegat && (velocitatActual > 0)){
-            velocitatActual -= 10;
-        }
-        printSpecs();
-    }
-
-    public void repararVehicle (){
-        System.out.println("El cotxe amb matrícula " + matricula + " està en reparació.");
-    }
+	public void repararVehicle() {
+		System.out.println("El cotxe amb matrícula " + matricula + " està en reparació.");
+	}
 
 }
