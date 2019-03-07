8f7ae4f247c8b46a5b746d9cf4c8fa91a691df1c
Neojate <albert15_89@hotmail.com>
2019-03-07 16:46:08 +0100

    ahoraconmain

---
+++ provaMain.java
@@ -0,0 +1,13 @@
+
+public class provaMain {
+
+	public static void main(String[] args) {
+		Vehicle vehicle = new Vehicle(100, "7765FHX");
+		vehicle.engega();
+		vehicle.acceleracio();
+		vehicle.desacceleracio();
+		vehicle.atura();
+
+	}
+
+}
