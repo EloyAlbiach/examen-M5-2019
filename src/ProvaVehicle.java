5347a0441e3c2962eefab35098dfd8da485e8458
Ruben <dakota.25@hotmail.com>
2019-03-07 16:48:41 +0100

    ProvaVehicle.java

---
+++ src/ProvaVehicle.java
@@ -0,0 +1,11 @@
+package Examen_Ex4;
+
+public class ProvaVehicle {
+
+	public static void main(String[] args) {
+		Vehicle vehicle = new Vehicle(100, "5533GHV");
+		vehicle.engega();
+		vehicle.acelerar();
+		vehicle.atura();
+	}
+}
