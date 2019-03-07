1f2042c77e733c76316a606716c93e12a25d637a
alberto <albertotiffon@gmail.com>
2019-03-07 16:43:48 +0100

    main

---
+++ ExamenUF2M5/src/MainVehicle.java
@@ -0,0 +1,13 @@
+
+public class MainVehicle {
+	public static void main(String[] args) {
+		Vehicle ve = new Vehicle(120, "6786 SJ");
+		ve.engega();
+		ve.acc();
+		ve.acc();
+		ve.dec();
+		ve.dec();
+		ve.atura();
+		ve.repararVehicle();
+	}
+}
