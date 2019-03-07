76735053b6b41d16de861b15d2286c9a4b79e364
Jneko <Jneko@gmail.com>
2019-03-07 16:40:52 +0100

    Primer refactor del projecto Vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -9,42 +9,46 @@ public class Vehicle {
         vLimit = velocitatLimit;
         matricula = matriculaCotxe;
     }
-
-    public void engega (){
-       engegat = true;
-        System.out.println("Estic engegat.");
+    
+    public void mensajes(int select) {
+    	if (select==0) System.out.println("M'he aturat");
+    	if (select==1) System.out.println("El cotxe amb matrícula " + matricula + " està en reparació.");
+    	if (select==2) estado();
+    }
+    
+    public void estado() {
+    	System.out.println("Estic engegat.");
         System.out.println("Velocitat actual = " + velocitatActual);
         System.out.println("Límit de velocitat = " + vLimit);
         System.out.println("Matrícula = " + matricula);
     }
 
+    public void engega (){
+       engegat = true;
+       mensajes(2);
+    }
+
     public void atura (){
         engegat = false;
-        System.out.println("M'he aturat");
+        mensajes(0);
     }
 
     public void acc (){
         if (engegat && ((velocitatActual + 10) < vLimit)){
             velocitatActual += 10;
         }
-        System.out.println("Estic engegat.");
-        System.out.println("Velocitat actual = " + velocitatActual);
-        System.out.println("Límit de velocitat = " + vLimit);
-        System.out.println("Matrícula = " + matricula);
+        mensajes(2);
     }
 
     public void dec (){
         if (engegat && (velocitatActual > 0)){
             velocitatActual -= 10;
         }
-        System.out.println("Estic engegat.");
-        System.out.println("Velocitat actual = " + velocitatActual);
-        System.out.println("Límit de velocitat = " + vLimit);
-        System.out.println("Matrícula = " + matricula);
+        mensajes(2);
     }
 
     public void repararVehicle (){
-        System.out.println("El cotxe amb matrícula " + matricula + " està en reparació.");
+    	mensajes(1);
     }
 
 }
